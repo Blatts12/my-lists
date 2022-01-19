b2cd6bb5d9d862168a9c86351d669ada52bfd1b7
Brofix <9402720+Blatts12@users.noreply.github.com>
2022-01-19 19:13:20 +0100

    added validation error for register

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
 import {
   IsEmail,
   IsString,
-  Length,
   Matches,
   MaxLength,
+  MinLength,
   Validate,
 } from 'class-validator';
 import { Unique } from 'src/validators/unique.validator';
@@ -11,17 +11,34 @@ import { User } from '../entities/user.entity';
 
 export class CreateUserDto {
   @IsString()
-  @Length(5, 64)
-  @Validate(Unique, [User])
+  @MaxLength(64, {
+    message: 'Username must be at most 64 characters',
+  })
+  @MinLength(5, {
+    message: 'Username must be at least 5 characters',
+  })
+  @Validate(Unique, [User], {
+    message: 'User with this username already exists',
+  })
   username: string;
 
   @IsEmail()
   @MaxLength(256)
-  @Validate(Unique, [User])
+  @Validate(Unique, [User], {
+    message: 'User with this email already exists',
+  })
   email: string;
 
   @IsString()
-  @Length(8, 64)
-  @Matches('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!#$%@&? "\']).+$')
+  @MaxLength(64, {
+    message: 'Password must be at most 64 characters',
+  })
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters',
+  })
+  @Matches('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!#$%@&? "\']).+$', '', {
+    message:
+      'Password must contain one small and big letter, one digit and one special character',
+  })
   password: string;
 }
