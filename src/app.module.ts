a0f4e28d65fad9b606a8a35a809a3acfb65cd941
Brofix <9402720+Blatts12@users.noreply.github.com>
2022-01-27 14:19:22 +0100

    added empty Items resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { ValidatorsModule } from './validators/validators.module';
 import TypeOrmConfig from './typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule,
     ValidatorsModule,
+    ItemsModule,
   ],
   controllers: [],
   providers: [],
